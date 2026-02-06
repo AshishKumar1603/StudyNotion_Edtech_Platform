4ae69f3bb460b0e0166a49766e55c086cf7851b5
Ashish Kumar <akgautam1603@gmail.com>
2026-02-06 10:34:55 +0530

    Update CORS middleware to allow multiple origins for improved accessibility

--- server/index.js
+++ server/index.js
@@ -29,7 +29,12 @@ const PORT = process.env.PORT || 4000;
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL || "http://localhost:3000"],
+    origin: [
+      process.env.FRONTEND_URL,
+      "http://localhost:3000",
+      "http://study-notion-edtech-platform-mu-virid.vercel.app",
+    ],
+
     credentials: true,
   }),
 );
